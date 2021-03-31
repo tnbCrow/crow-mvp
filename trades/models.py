7135ba9ad5f210c21d043bb21225e2c5f2d0b93f
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-03-31 08:59:30 +0545

    created CompletedTrades model

--- trades/models.py
+++ trades/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class CompletedTrades(models.Model):
+    buyer = models.CharField(max_length=63)
+    seller = models.CharField(max_length=63)
+    amount = models.PositiveBigIntegerField()
+    rate = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.buyer}-{self.seller}:{self.amount}@{self.rate}'
+    
