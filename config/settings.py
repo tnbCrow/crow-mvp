4d3e0db1d5b489f645ff2904c912adaef7880f71
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-04-25 18:05:56 +0545

    debug false uff

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(24))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'tnbcrow.pythonanywhere.com']
 
