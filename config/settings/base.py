dcd00ad777f51b04135349c29cb3940fa4994169
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-12-03 12:25:47 +0545

    allow all in ALLOWED_HOST

--- config/settings/base.py
+++ config/settings/base.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(24))
 
-ALLOWED_HOSTS = ['127.0.0.1', 'tnbcrow.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
